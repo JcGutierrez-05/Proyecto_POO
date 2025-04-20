82f81dcd7825587d7ac57b462539eb09c18e4634
JcGutierrez-05 <juancarlosgutierrez2508@gmail.com>
2025-04-19 20:02:40 -0500

    creacion

---
+++ pro_miscelania/service.py
@@ -0,0 +1,12 @@
+from pro_miscelania.models import Categoria
+from pro_miscelania.models import Ciudad
+from pro_miscelania.models import Cliente
+from pro_miscelania.models import Compra
+from pro_miscelania.models import Envios
+from pro_miscelania.models import FacturaCompraProducto
+from pro_miscelania.models import FacturaProducto
+from pro_miscelania.models import FacturaVenta
+from pro_miscelania.models import Producto
+from pro_miscelania.models import Proveedor
+
+def 
