774a384b890b006c4a612085d6608eb3db81d27a
Alejandro Peña Restrepo <alejoperes2003@gmail.com>
2025-04-19 20:33:14 -0500

    primera funcionalidad

--- pro_miscelania/service.py
+++ pro_miscelania/service.py
@@ -9,4 +9,17 @@ from pro_miscelania.models import FacturaVenta
 from pro_miscelania.models import Producto
 from pro_miscelania.models import Proveedor
 
-def 
+class ClienteFuncionalidad:
+    @staticmethod
+    def crear_cliente(nombre, email, telefono, direccion):
+        if Cliente.objects.filter(cliente_mail=email).exists():
+            print("Error: El correo ya está registrado")
+        else:
+            cliente = Cliente.objects.create(
+                cliente_nom=nombre,
+                cliente_mail=email,
+                cliente_tel=telefono,
+                cliente_direc=direccion
+            )
+            cliente.save()
+            print(f"Usuario {nombre} fue creado exitosamente")
